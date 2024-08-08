c8231cc653c5287f1037d1635fb762c726fe9177
jeanFrancoGuerreroAcero <guerreroacerojeanfranco@gmail.com>
2024-08-08 08:21:31 -0500

    :sparkles: trabajo de Readme con ejercico en Python

---
+++ Readme.md
@@ -0,0 +1,44 @@
+# TRABAJO DE README CON EJERCICIO PYTHON
+Se realizo un ejercicio en donde se debia realizar un software para una panaderia llamada panCamp
+
+## General
+- Descripcion: El software permite navegar entre si mediante menus que muestran opciones especificas, el programa cuenta opciones de:
+   1. ventas:la panaderia puede realizar ventas donde resgistra la fecha de la venta, informacion del cliente y la de el empleado que lo atendio.
+
+   2. compra:la panaderia hace compras a distribuidores en donde gyuarda toda la infotmacion acerca de el producto que compro y de el proovedor que se lo vendio.
+
+- uso: mediante las opciones que se da en los menus
+
+## Funcionamiento
+- Menus: cuenta con menus interactivos en donde estos le especifican al usuario las opciones disponibles dentro del programa 
+
+## Lenguajes usados
+
+- Python
+
+## Estructura del proyecto
+
+![alt text](<Captura de pantalla 2024-08-08 022636.png>)
+
+## Caracteristicas
+1. cuenta con 3 json: 
+
+- json de info- cuenta con la informacion de los productos que tiene la panaderia 
+
+- json de ventas-cuenta con la informacion de el cliente que compro el producto y del empleado que realizo la venta 
+
+- json de compras-cuenta con la informacion del propvedor que surtio a la panaderia y de los productos que se compraron 
+
+- el archivo de main.py contiene el funcionamiento del software
+
+## Diseño
+
+- El diseño del software fue definido por una serie de requisitos propuestos en Notion 
+
+## Instrucciones de uso
+
+- ejecutar el programa desde la terminal de visual code o instalar la extension llamada code runer 
+
+## Desarrolado por
+
+- jean franco guerrero acero, estudiante de Campusalands como trabajo de Readme con ejercicio Python
--- main.py
+++ main.py
@@ -13,12 +13,40 @@ def guardarArchivo(miData):
     with open("./info.json","w") as outfile:
         json.dump(miData,outfile)
 
+##################################
+#json de compras#
+
+def abierto():
+    jeisiton=[]
+    with open('./compras.json','r') as openfile:
+        jeisiton= json.load(openfile)
+    return jeisiton
+
+def archivoguardado(miData):
+    with open("./compras.json","w") as outfile:
+        json.dump(miData,outfile)
+
+#######################################
+#json de ventas
+
+def inaugurado():
+    jisiton2=[]
+    with open('./ventas.json','r') as openfile:
+        jisiton2= json.load(openfile)
+    return jisiton2
+
+def guardado2(miData):
+    with open("./ventas.json","w") as outfile:
+        json.dump(miData,outfile)
+
+
 print("---------------------¬")
 print("BIENVENIDO A PANCAMP")
 print("----------------------")
 print("")
 print("1. ver reportes")
 print("2. realizar una compra")
+print("3. realizar una venta")
 print("")
 opcion=input("¿Que opcion deseas realizar?: ")
 ciclo=True
@@ -32,22 +60,146 @@ while ciclo:
         print("")
         print("Que reporte desea visualizar")
         print("1. ventas")
-        print("2. compras")
+        print("2. stock")
         quereporte=input("")
         if quereporte=="1":
-            jsoon=abrirArchivo()
+            jsoon=inaugurado()
             print("-----------------")
             print("Reporte de ventas")
             print("-----------------")
-            for i in jsoon ["Panaderia"]:
+            for i in jsoon:
                 contador=0
-                print("|ID: ",i)
-                print("|NOMBRE: ",i)
-                print("|FECHA DE VENTA: ",i)
-                print("|")
-    
-
+                print("|ID: ",i["id"])
+                print("|NOMBRE CLIENTE: ",i["nombre"])
+                print("|FECHA DE VENTA: ",i["fecha"])
+                print("|PRODUCTO: ",i["producto"])
+                print("|CANTIDAD: ",i["cantidad"])
+                print("|PRECIO: ",i["precio"])
                 ciclo=False
+        if quereporte=="2":
+            jsoonStock=abrirArchivo()
+            print("-----------------")
+            print("Reporte de stock")
+            print("-----------------")
+            for i in jsoonStock["Panaderia"]:
+                contador=0
+                print("------------------")
+                print("stock de panaderia")
+                print("------------------")
+                print("|NOMBRE: ",i["producto"])
+                print("|CANTIDAD: ",i["cantidad"])
+                print("|PRECIO :",i["precio"])
+                print("")
+            for i in jsoonStock["Pasteleria"]:
+                contador=0
+                print("------------------")
+                print("stock de Pasteleria")
+                print("------------------")
+                print("|NOMBRE: ",i["producto"])
+                print("|CANTIDAD: ",i["cantidad"])
+                print("|PRECIO :",i["precio"])
+                print("")
+            for i in jsoonStock["Bebidas"]:
+                contador=0
+                print("------------------")
+                print("stock de Bebidas")
+                print("------------------")
+                print("|NOMBRE: ",i["producto"])
+                print("|CANTIDAD: ",i["cantidad"])
+                print("|PRECIO :",i["precio"])
+                print("")
+            for i in jsoonStock["Apartado de promociones"]:
+                contador=0
+                print("------------------")
+                print("stock de Apartado de promociones")
+                print("------------------")
+                print("|NOMBRE: ",i["producto"])
+                print("|CANTIDAD: ",i["cantidad"])
+                print("|PRECIO :",i["precio"])
+                print("")
+    if opcion=="2":
+        print("----------------------------------")
+        print("BIENVENIDO A LA SECCION DE COMPRAS")
+        print("----------------------------------")
+        print("")
+        jsoonCompras=abierto()
+        print("LISTA DE PROOVEDORES")
+        print("1 -julio cesar")
+        print("2 -jhon alexander")
+        print("3 -danna gomez")
+        print("4 -franklin leonardo")
+        print("5 -sebastain juan")
+        print("")
+        print("ingresa el id de proovedor que eres")
+        queProovedor=int(input("¿Que proovedor eres? :"))
+        queProducto=(input("¿Que producto se va a comprar? :"))
+        jsoonCompras[queProovedor-1]["producto"]=queProducto
+        archivoguardado(jsoonCompras)
+
+        queCantidad=input("¿Que cantidad se va a comprar?: ")
+        jsoonCompras[queProovedor-1]["cantidad"]=queProducto
+        archivoguardado(jsoonCompras)
+
+        quePrecio=input("¿Que precio tiene la compra?: ")
+        jsoonCompras[queProovedor-1]["precio"]=queProducto
+        archivoguardado(jsoonCompras)
+
+        print("ahora dame tu informacion")
+        queNombre=input("cual es tu nombre: ")
+        jsoonCompras[queProovedor-1]["nombre"]=queProducto
+        archivoguardado(jsoonCompras)
+
+        queContacto=input("cual es tu numero de contacto: ")
+        jsoonCompras[queProovedor-1]["contacto"]=queProducto
+        archivoguardado(jsoonCompras)
+
+        queFecha=input("en que fecha se va a realizar la compra: ")
+        jsoonCompras[queProovedor-1]["fecha"]=queProducto
+        archivoguardado(jsoonCompras)
+        ciclo=False
+
+    if opcion=="3":
+        print("----------------------------------")
+        print("BIENVENIDO A LA SECCION DE VENTAS")
+        print("----------------------------------")
+        print("")
+        jsoonVentas=inaugurado()
+        queVenta=int(input("en la factura de la venta entre el 1 y el 5 que id se te dio: "))
+        queFecha2=input("en que fecha se realizo la venta: ")
+        jsoonVentas[queVenta-1]["fecha"]=queFecha2
+        guardado2(jsoonVentas)
+
+        queNombre2=input("cual es tu nombre: ")
+        jsoonVentas[queVenta-1]["nombre"]=queNombre2
+        guardado2(jsoonVentas)
+
+        queDireccion2=input("cual es tu direccion: ")
+        jsoonVentas[queVenta-1]["direccion"]=queDireccion2
+        guardado2(jsoonVentas)
+        
+        queEmpleado2=input("cual es el nombre del empleado: ")
+        jsoonVentas[queVenta-1]["empleado"]=queEmpleado2
+        guardado2(jsoonVentas)
+
+        queCargo2=input("cual es el cargo que tiene: ")
+        jsoonVentas[queVenta-1]["cargo"]=queCargo2
+        guardado2(jsoonVentas)
+
+        queProducto2=input("cual es el producto: ")
+        jsoonVentas[queVenta-1]["producto"]=queProducto2
+        guardado2(jsoonVentas)
+
+        queCantidad2=input("cual es la cantidad: ")
+        jsoonVentas[queVenta-1]["cantidad"]=queCantidad2
+        guardado2(jsoonVentas)
+
+        quePrecio2=input("cual es el precio: ")
+        jsoonVentas[queVenta-1]["precio"]=quePrecio2
+        guardado2(jsoonVentas)
+        ciclo=False
+
+        
+
 
 
 
